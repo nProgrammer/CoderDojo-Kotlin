5d9e20382316b1d5bd2e259a4df60c7654520cb3
Norbert Wagner <wagner.norbert277@gmail.com>
2019-11-09 12:04:58 +0100

    1 st commit

---
+++ app/src/main/java/pl/norbertwagner/nprogrammer/coderdojozajecia1praktyka/MainActivity.kt
@@ -0,0 +1,19 @@
+package pl.norbertwagner.nprogrammer.coderdojozajecia1praktyka
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun saveBTRClick(v: View) {
+        val text = NormalPlainText.text.toString()
+        textView.text = text
+    }
+}
